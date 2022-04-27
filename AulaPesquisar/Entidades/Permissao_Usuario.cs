83f826c05697da35e778f43a67f9af52a13fa78e
GABRIEL MUNHOZ <gabriel.munhoz01@fatec.sp.gov.br>
2022-04-27 10:55:06 -0300

    quase pronto

---
+++ AulaPesquisar/Entidades/Permissao_Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaPesquisar.Entidades
+{
+    public class Permissao_Usuario
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int PermissaoId { get; set; }
+        public Permissao Permissao { get; set; }
+        public Usuarios Usuario { get; set; }
+
+
+
+    }
+}
---
+++ AulaPesquisar/Entidades/Usuarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaPesquisar.Entidades
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public string Login  { get; set; }
+        public string Senha { get; set; }
+        public string NomeCompleto { get; set; }
+        public string Email { get; set; }
+
+    }
+}
